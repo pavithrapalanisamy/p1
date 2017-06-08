464f58b4e353542243e48b8df5da557d9311bae7
pavithrapalanisamy <p.pavithra12349@gmail.com>
2017-06-08 09:58:08 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class Even
+{
+public static void main(String args[])
+{
+Scanner input=new scanner(System.in);
+int a;
+if(a%2)==0)
+System.out.println("the no is even no");
+else
+System.out.println("the no is odd no");
+}
+}
